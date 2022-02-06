7549f7fb3688a05faa2fd643aa6533ee087176ba
Kithinga <menscfesz@gmail.com>
2022-02-07 02:48:52 +0300

    my firdt commit

---
+++ one.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+</head>
+
+<body>
+    <div class="header">
+        <div>
+            <li>one</li>
+        </div>
+    </div>
+    <p>many more men</p>
+    <div>i almost gave up and then the lord came for my rescue</div>
+    <h6>we shoulkd also learn things, i mean new things yeah</h6>
+</body>
+
+</html>
