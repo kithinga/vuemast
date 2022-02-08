b0fce2ceba94203bcb304715c3e7a872c1f4a418
Kithinga <menscfesz@gmail.com>
2022-02-08 17:11:23 +0300

    laracasting

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
       lastName: "Mutuku",
       email: "kithinga@love.com",
       gender: "Male",
-      picture: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.joeyl.com%2Fblog%2Fall%2Fpost%2Fzombie-boy-rick-genest-photoshoot&psig=AOvVaw2Xywm6x4Qt2Dl8jNmmik5Z&ust=1644408369054000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLCunOeI8PUCFQAAAAAdAAAAABAD",
+      picture: "images/images.jpeg"
     };
   },
 });
