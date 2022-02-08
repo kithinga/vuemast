e448f9445c118a476a1f11654040c6a464c97f47
Kithinga <menscfesz@gmail.com>
2022-02-08 15:23:58 +0300

    yutu

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const app = new Vue({
-     data(){
-          return{
-               firstName:'John',
-          }
-     },
-})
+const app = Vue.createApp({
+  data() {
+    return {
+      firstName: "John",
+      lastName: "John",
+      lastName: "kithinga@love.com",
+      gender: "Male",
+      picture: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.joeyl.com%2Fblog%2Fall%2Fpost%2Fzombie-boy-rick-genest-photoshoot&psig=AOvVaw2Xywm6x4Qt2Dl8jNmmik5Z&ust=1644408369054000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLCunOeI8PUCFQAAAAAdAAAAABAD",
+    };
+  },
+});
