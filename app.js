5e9378a64dbf6f234499c4d5f092d950b7b024d9
Kithinga <menscfesz@gmail.com>
2022-02-08 21:58:19 +0300

    look here festus

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const app = Vue.createApp({
       lastName: "Mutuku",
       email: "kithinga@love.com",
       gender: "female",
-      picture: "images/images.jpeg"
+      picture: "images/images.jpeg",
     };
   },
+  methods:{
+    getUser(){
+      this.firstName = "Buelik",
+      this.lastName = "Yong",
+      this.email = "kithinga@love.com",
+      this.gender = "female",
+      this.picture = "images/images.jpeg",
+      console.log("Welcome to vue development Mr. " + " "+ this.firstName);
+    }
+  }
 });
 
 app.mount('#app')
