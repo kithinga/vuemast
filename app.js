af79013250199e6416df79c6529751627b98326b
Kithinga <menscfesz@gmail.com>
2022-02-09 12:42:11 +0300

    pseudo

--- app.js
+++ app.js
@@ -12,14 +12,13 @@ const app = Vue.createApp({
     async getUser(){
       const res = await fetch('https://randomuser.me/api')
       const {results} = await res.json()
-      console.log(results)
-
-      this.firstName = "Buelik",
-      this.lastName = "Yong",
-      this.email = "kithinga@love.com",
-      this.gender = "male",
-      this.picture = "images/images.jpeg",
-      console.log("Welcome to vue development Mr. " + " "+ this.firstName);
+     
+      this.firstName = results[0].name.first
+      this.lastName = results[0].name.last
+      this.email = results[0].email
+      this.gender = results[0].gender
+      this.picture = results[0].picture.large
+   
     }
   }
 });
